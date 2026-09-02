98a96ac5176addc54d0dc114d9b32d25623f70af
maramasot <mikel.arambarri@tartanga.eus>
2026-09-02 12:37:33 +0200

    Tercer commit con borrados de texto

--- JavaGitFroga/src/javagitfroga/JavaGitFroga.java
+++ JavaGitFroga/src/javagitfroga/JavaGitFroga.java
@@ -17,7 +17,6 @@ public class JavaGitFroga {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
-        System.out.println("Esto se va a borrar");
     }
     
 }
